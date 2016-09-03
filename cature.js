63da980cc815fa90de235111296336a8368de7d5
Caesar <gitter@liftm.de>
2016-09-03 15:45:40 +0200

    Update to new version of steamgifts

--- cature.js
+++ cature.js
@@ -20,41 +20,43 @@ phantom.onError = PrintStackTrace;
 
 var page = require('webpage').create();
 
-//page.onResourceReceived = function(response) {
-//	if (response.stage !== "end") return;
-//		console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + response.url);
-//};
-//page.onResourceRequested = function(requestData, networkRequest) {
-//	console.log('Request (#' + requestData.id + '): ' + requestData.url);
-//};
-//page.onUrlChanged = function(targetUrl) {
-//	console.log('New URL: ' + targetUrl);
-//};
-//page.onLoadFinished = function(status) {
-//	console.log('Load Finished: ' + status);
-//	if(status == 'fail')
-//		page.render('load-fail.png');
-//};
-//page.onLoadStarted = function() {
-//	console.log('Load Started');
-//};
-//page.onNavigationRequested = function(url, type, willNavigate, main) {
-//	console.log('Trying to navigate to: ' + url);
-//};
+
+/*page.onResourceReceived = function(response) {
+	if (response.stage !== "end") return;
+		console.log('Response (#' + response.id + ', stage "' + response.stage + '"): ' + response.url);
+};
+page.onResourceRequested = function(requestData, networkRequest) {
+	console.log('Request (#' + requestData.id + '): ' + requestData.url);
+};
+page.onUrlChanged = function(targetUrl) {
+	console.log('New URL: ' + targetUrl);
+};
+page.onLoadFinished = function(status) {
+	console.log('Load Finished: ' + status);
+	if(status == 'fail')
+		page.render('load-fail.png');
+};
+page.onLoadStarted = function() {
+	console.log('Load Started');
+};
+page.onNavigationRequested = function(url, type, willNavigate, main) {
+	console.log('Trying to navigate to: ' + url);
+};*/
 
 var CS = {};
 function Start() {
 	var fs = require('fs');
 	setTimeout(function() {
 		page.render("timeout.png");
+		console.log(page.evaluate(function() { return document.documentElement.innerHTML; }));
 		ErrorOut("Overall timeout!");
-	}, 10 * 60 * 1000);
+	}, 15 * 60 * 1000);
 	CS = JSON.parse(fs.read('settings'));
 	page.viewportSize = { width: 1920, height: 1500 };
 	phantom.cookiesEnabled = true;
 	page.settings.userAgent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)';
 	Log("Starting…");
-	Open('http://www.steamgifts.com', OnMainOpen);
+	Open('https://www.steamgifts.com', OnMainOpen);
 }
 
 function OnMainOpen(status) {
@@ -77,18 +79,30 @@ function OnLoginOpen(status) {
 	page.evaluate(function(nm, pw) {
 		document.getElementById('steamAccountName').value = nm;
 		document.getElementById('steamPassword').value = atob(atob(atob(atob(pw)))); // No, this is not secure or anything. But I'm reluctant to put it in plain text.
+		document.getElementById('loginfriendlyname').value = 'phantomjs / steamgifts / cature.js';
 	}, CS.SNM, CS.SPW);
 	page.render('steam-login.png');
 	var fac2ivl = setInterval(function() {
-		if(page.evaluate(function() { return document.getElementById('authcode') != null; })) {
+		var needac = page.evaluate(function() { 
+			var ac = document.getElementById('authcode'); 
+			if(!ac) throw "cature.js needs an update";
+			while(ac) {
+				if(ac.style && ac.style.display == 'none')
+					return false;
+				ac = ac.parentNode;
+			}
+			return true;
+		});
+		if(needac) {
 			WarnIn("Authcode requested, can not continue.");
 			var system = require('system');
 			system.stdout.write('Enter Steam Authcode: ');
 			system.stdout.flush();
 			var ac = system.stdin.readLine();
-			if(ac.trim() == '')
+			if(ac.trim() == '') {
+				page.render('steam-empty-authcode.png');
 				ErrorOut("Empty authcode given, aborting.");
-			else {
+			} else {
 				page.evaluate(function(ac) {
 					document.getElementById('authcode').value = ac;
 					document.getElementById('friendlyname').value = 'phantomjs / steamgifts / cature.js';
@@ -104,9 +118,10 @@ function OnLoginOpen(status) {
 					document.querySelector('#auth_buttonset_entercode .leftbtn').dispatchEvent(ev);
 				}, ac);
 				Log("Authcode sent, abort manually if this takes too long.");
+				setTimeout(function() { page.render('steam-authcode.png'); }, 20000);
 			}
+			clearInterval(fac2ivl);
 		}
-		clearInterval(fac2ivl);
 	}, 500);
 	var olf = page.onLoadFinished;
 	page.onLoadFinished = function(status) {
@@ -117,16 +132,32 @@ function OnLoginOpen(status) {
 			page.onLoadFinished = olf;
 			OnSteamgiftsLogin();
 		} else {
-			var wrong = function() {
-				var icc = document.querySelector('#auth_message_incorrectcode');
-				return !(icc && icc.style.display == 'none');
-			};
-			if(wrong)
+			var wrong = page.evaluate(function() {
+				var icc = document.querySelectorAll('.auth_buttonset');
+				if (icc.length == 0)
+					throw "cature.js needs updating.";
+				for(var i = 0; i < icc.length; i++) {
+					var e = icc[i];
+					if (e.style.display != 'none' && e.id.match(/incorrectcode/))
+						return e.outerHTML;
+				}
+				return false;
+			});
+			//require('system').stdout.write(page.evaluate(function() { return document.documentElement.innerHTML; }));
+			if(wrong) {
+				// console.log(wrong);
 				ErrorOut("Wrong authcode.");
+			} else
+				page.evaluate(function() {
+					document.getElementById('success_continue_btn').click();
+				});
 		}
 	};
 	page.evaluate(function() {
-		document.getElementById('imageLogin').click();
+		setTimeout(function() { 
+			document.getElementById('imageLogin').click(); 
+			console.log("Login button has been mashed.");
+		}, 1000);
 	});
 }
 
@@ -158,7 +189,7 @@ function GrepCycle(nextpage, giveaways) {
 		}
 		nextact();
 	} else {
-		Open('http://www.steamgifts.com/giveaways/search?page=' + page, GrepCycle.bind(undefined , nextpage + 1, giveaways));
+		Open('https://www.steamgifts.com/giveaways/search?page=' + page, GrepCycle.bind(undefined , nextpage + 1, giveaways));
 	}
 }
 
@@ -193,7 +224,7 @@ function AvailablePoints() {
 }
 
 function IsSteamGifts() {
-	return page.url.match(RegExp('^http://[^/]*steamgifts'));
+	return page.url.match(RegExp('^https?://[^/]*steamgifts'));
 }
 
 function IsSteamGiftsLoggedIn() {
@@ -210,9 +241,15 @@ function EnterableGiveaways(known) {
 				if(g.getElementsByClassName('giveaway__row-inner-wrap')[0].classList.contains('is-faded'))
 					return undefined;
 				var l = g.getElementsByClassName('giveaway__heading__name')[0];
-				if(!l.href.match(RegExp('^http://[^/]*/giveaway/')))
-					throw "Structures unexpected: Giveaway is not a giveaway";
-				var ent = parseInt(g.getElementsByClassName('giveaway__links')[0].innerHTML.match(/([0-9,]*) entries/)[1].split(',').join(''));
+				if(!l.href.match(RegExp('^https?://[^/]*/giveaway/')))
+					throw Error("Structures unexpected: Giveaway is not a giveaway (is " + l.href + " instead)");
+				var glinks = g.getElementsByClassName('giveaway__links');
+				if (!glinks || glinks.length < 1)
+					throw Error("Entry count deduction, fail 1");
+				var entmatch = glinks[0].innerHTML.match(/([0-9,]*) entries/);
+				if (!entmatch || entmatch.length != 2)
+					throw Error("Entry count deduction, fail 2");
+				var ent = parseInt(entmatch[1].split(',').join(''));
 				var copies = 1, points = undefined;
 				Array.prototype.map.call(g.getElementsByClassName('giveaway__heading__thin'), function(h) {
 					var mp = g.innerHTML.match(/\(([0-9]+)(P| points)\)/);
@@ -223,11 +260,10 @@ function EnterableGiveaways(known) {
 						copies = parseInt(mc[1]);
 				});
 				if(points === undefined)
-					throw "Structures unexpected: Giveaway is not a giveaway";
+					throw "Structures unexpected: Giveaway is not a giveaway - no points";
 				return { u: l.href, c: copies, p: points, e: ent };
 			} catch(e) {
-				WarnIn("Error scraping giveaways: " + e.message);
-				return undefined;
+				throw("Error scraping giveaways: " + e.message);
 			}
 		}).filter(function(a) { return a != undefined; });
 	}).concat(known).uniqueOn(function(e) { return e.u; });
@@ -257,6 +293,7 @@ function GiveawayState() {
 
 function ErrorOut(msg) {
 	Log('ERROR: ' + msg);
+	page.render('cature-error.png');
 	debugger;
 	Exit(-1);
 }
@@ -279,6 +316,7 @@ function Exit(code) {
 
 function Log(msg) {
 	var d = new Date();
+	msg = msg.replace(/\s+$/, '');
 	console.log('['
 		+ zeroExtend(d.getHours(), 2) + ':' + zeroExtend(d.getMinutes(), 2) + ':' + zeroExtend(d.getSeconds(), 2)
 		+ '] ' + msg);
@@ -291,14 +329,16 @@ page.onResourceError = function(resourceError) {
 };
 
 function Open(uri, action) {
-	var retry = 3;
+	var maxretry = 3;
+	var retry = maxretry;
 	var tryl;
 	tryl = function(w) {
 		setTimeout(function() {
 			page.open(uri, function(status) {
 				retry--;
 				if (status !== 'success') {
-					WarnIn('Unable to load url: ' + page.reason_url + ' - ' + page.reason + ' Retrys left: ' + retry);
+					if(retry <= maxretry / 2)
+						WarnIn('Unable to load url: ' + page.reason_url + ' - ' + page.reason + ' - Retry' + retry + '/' + maxretry);
 					if(retry > 0)
 						tryl(5678);
 					else
@@ -352,7 +392,7 @@ function zeroExtend(n, l) {
 }
 
 page.onConsoleMessage = function(msg) {
-	Log('PAGE: ' + msg);
+	Log(msg);
 };
 
 Start();
